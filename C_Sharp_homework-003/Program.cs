d74d977001882f247292cb6b1b94fb9df515ffb0
maxim <horbatyukitstep@gmail.com>
2022-02-13 09:54:05 +0200

    Add project files.

---
+++ C_Sharp_homework-003/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_homework_003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
